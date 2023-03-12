81f36ed0af146ac28e4fb7a12811c6c75da75294
criston <cris10lobo@gmail.com>
2023-03-12 13:40:46 +0530

    feat(vault): added route to KDC

--- frontend/src/pages/Vault.vue
+++ frontend/src/pages/Vault.vue
@@ -1,28 +1,50 @@
 <template>
   <div class="h-full w-full">
-    <pre>{{ $resources.posts }}</pre>
     <div class="h-full flex flex-col justify-center items-center">
-      <Button @click="$resources.genrateQR()">Click to generate QR</Button>
+      <Button @click="generateQR()">Get Resource</Button>
+      <pre>{{ token }}</pre>
     </div>
-  </div> 
+  </div>
 </template>
 
 <script>
-import { computeStyles } from '@popperjs/core';
-import { Button } from 'frappe-ui';
-
-
+import { Button } from "frappe-ui";
 
 export default {
-    name: "Vault",
-    resources: {
-       genrateQR() {
-        return {
-        url: 'https://jsonplaceholder.typicode.com/posts/1',
-        method: "GET",
-      }
+  name: "Vault",
+  // eslint-disable-next-line vue/no-reserved-component-names
+  components: { Button },
+  data: () => ({
+    token: null,
+  }),
+  methods: {
+    async generateQR() {
+      try {
+        const res = await fetch(`http://localhost:5000/api/session/initiate`);
+
+        if (!res.ok) {
+          const message = `An error has occured: ${res.status} - ${res.statusText}`;
+          throw new Error(message);
         }
+
+        const data = await res.json();
+
+        const result = {
+          status: res.status + "-" + res.statusText,
+          headers: {
+            "Content-Type": res.headers.get("Content-Type"),
+            "Content-Length": res.headers.get("Content-Length"),
+          },
+          length: res.headers.get("Content-Length"),
+          data: data,
+        };
+
+        console.log(result);
+        this.token = result.data.data;
+      } catch (err) {
+        console.log(err.message);
+      }
     },
-    components: { Button }
+  },
 };
 </script>
