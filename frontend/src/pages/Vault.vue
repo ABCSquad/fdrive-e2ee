d4f171019b46631d1a9f0c87164159664e8ae0e8
BryanRonad <bryanronad@gmail.com>
2023-04-02 16:27:08 +0530

    feat: encrypting message

--- frontend/src/pages/Vault.vue
+++ frontend/src/pages/Vault.vue
@@ -11,6 +11,9 @@
 <script>
 import { Button } from "frappe-ui";
 import QrcodeVue from "qrcode.vue";
+import { Buffer } from "buffer";
+import { uuid } from "vue-uuid";
+import SignalProtocolStore from "libsignal-protocol/test/InMemorySignalProtocolStore.js";
 
 export default {
   name: "Vault",
@@ -53,7 +56,6 @@ export default {
           data: data,
         };
 
-        console.log(result);
         this.token = result.data.data;
 
         // Connect to websocket connection
@@ -72,10 +74,135 @@ export default {
           }
           // If message is a prekey bundle
           if (receivedMessage.type === "primaryPreKeyBundle") {
-            console.log("Generating IdentityKey", receivedMessage.preKeyBundle);
-            const identityKey =
-              await window.libsignal.KeyHelper.generateIdentityKey();
-            console.log("IdentityKey", identityKey);
+            // Create a new signalStore
+            const signalStore = new SignalProtocolStore();
+
+            // Get primary device related data
+            const primarySignalProtocolAddress =
+              window.libsignal.SignalProtocolAddress.fromString(
+                receivedMessage.primarySignalProtocolAddress
+              );
+            let primaryPreKeyBundle = receivedMessage.preKeyBundle;
+            // Convert all keys in preKeyBundle to Buffers
+            primaryPreKeyBundle.identityKey = Buffer.from(
+              primaryPreKeyBundle.identityKey,
+              "base64"
+            ).buffer;
+            primaryPreKeyBundle.signedPreKey.publicKey = Buffer.from(
+              primaryPreKeyBundle.signedPreKey.publicKey,
+              "base64"
+            ).buffer;
+            primaryPreKeyBundle.signedPreKey.signature = Buffer.from(
+              primaryPreKeyBundle.signedPreKey.signature,
+              "base64"
+            ).buffer;
+            primaryPreKeyBundle.preKey.publicKey = Buffer.from(
+              primaryPreKeyBundle.preKey.publicKey,
+              "base64"
+            ).buffer;
+
+            // Save primary device identity in signalStore
+            signalStore.saveIdentity(
+              primarySignalProtocolAddress.toString(),
+              primaryPreKeyBundle.identityKey
+            );
+
+            // Functions
+            const generateIdentity = async () => {
+              const result = await Promise.all([
+                window.libsignal.KeyHelper.generateIdentityKeyPair(),
+                window.libsignal.KeyHelper.generateRegistrationId(),
+              ]);
+              return result;
+            };
+
+            // Create necessary compaion related data
+            // Signal Protocol Address
+            const signalProtocolAddress =
+              new window.libsignal.SignalProtocolAddress(
+                receivedMessage.primaryUsername,
+                uuid.v4().toString()
+              );
+            // identityKeyPair and registrationId
+            const [identityKeyPair, registrationId] = await generateIdentity();
+            // preKey
+            const preKeyId = Math.ceil(Math.random() * 100000);
+            const preKey = await window.libsignal.KeyHelper.generatePreKey(
+              preKeyId
+            );
+            // signedPreKey
+            const signedPreKey =
+              await window.libsignal.KeyHelper.generateSignedPreKey(
+                identityKeyPair,
+                registrationId
+              );
+
+            // Save companion related data to signalStore
+            signalStore.saveIdentity(
+              signalProtocolAddress.toString(),
+              identityKeyPair.pubKey
+            );
+            signalStore.storePreKey(preKey.keyId, preKey);
+            signalStore.storeSignedPreKey(signedPreKey.keyId, signedPreKey);
+
+            const sessionBuilder = new window.libsignal.SessionBuilder(
+              signalStore,
+              primarySignalProtocolAddress
+            );
+            const sessionPromise =
+              sessionBuilder.processPreKey(primaryPreKeyBundle);
+
+            sessionPromise.then(() => {
+              console.log("Sending initial message to primary...");
+              const sessionCipher = new window.libsignal.SessionCipher(
+                signalStore,
+                primarySignalProtocolAddress
+              );
+              let plaintextMessage = new TextEncoder().encode(
+                "Hey there, primary!"
+              ).buffer;
+              sessionCipher
+                .encrypt(plaintextMessage)
+                .then((ciphertext) => {
+                  const preKeyWhisperMessageToSend = {
+                    type: "preKeyWhisperMessage",
+                    preKeyWhisperMessage: ciphertext,
+                  };
+                  console.log(ciphertext);
+                  // Creating store for bob
+                  const bobSignalStore = new SignalProtocolStore();
+                  // Saving identity
+                  bobSignalStore.saveIdentity(
+                    primarySignalProtocolAddress.toString(),
+                    primaryPreKeyBundle.identityKey
+                  );
+                  // Create receiver sessionCipher
+                  const bobSessionCipher = new window.libsignal.SessionCipher(
+                    bobSignalStore,
+                    signalProtocolAddress
+                  );
+                  // Decrypting to test
+                  bobSessionCipher
+                    .decryptPreKeyWhisperMessage(ciphertext.body, "binary")
+                    .then((plaintext) => {
+                      console.log("Decrypted message: ", plaintext);
+                    })
+                    .catch((err) => {
+                      console.log("Error decrypting message");
+                      console.log(err);
+                    });
+                  socket.send(JSON.stringify(preKeyWhisperMessageToSend));
+                })
+                .catch((err) => {
+                  console.log("Error encrypting message");
+                  console.log(err);
+                });
+            });
+
+            sessionPromise.catch((err) => {
+              console.log("Session Promise Rejected");
+              console.log(err);
+            });
           }
         };
         socket.onclose = (event) => {
